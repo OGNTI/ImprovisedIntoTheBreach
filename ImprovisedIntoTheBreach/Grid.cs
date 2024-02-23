b893dc94828d6547166a68ce6ad1b2b9ea37d649
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2024-02-23 11:08:39 +0100

    iugaebuogad

---
+++ ImprovisedIntoTheBreach/Grid.cs
@@ -0,0 +1,40 @@
+﻿namespace ImprovisedIntoTheBreach;
+
+public class Grid
+{
+    protected Vector2 _position;
+    protected int _cols;
+    protected int _rows;
+    protected int _cellSize;
+    public Vector2[,] actualgrid;
+
+
+    public Grid(Vector2 position, int cols, int rows, int cellSize)
+    {
+        _cols = cols;
+        _rows = rows;
+        _cellSize = cellSize;
+        _position = position - new Vector2(_cols / 2 * _cellSize, _rows / 2 * _cellSize);
+        actualgrid = new Vector2[_cols, _rows];
+
+        for (int i = 0; i < _cols; i++)
+        {
+            for (int j = 0; j < _rows; j++)
+            {
+                actualgrid[i, j] = new Vector2((int)_position.X + (i * _cellSize), (int)_position.Y + (j * _cellSize));
+            }
+        }
+    }
+
+
+    public void Draw()
+    {
+        for (int i = 0; i < _cols; i++)
+        {
+            for (int j = 0; j < _rows; j++)
+            {
+                Raylib.DrawRectangleLines((int)_position.X + (i * _cellSize), (int)_position.Y + (j * _cellSize), _cellSize, _cellSize, Color.Black);
+            }
+        }
+    }
+}
--- ImprovisedIntoTheBreach/Program.cs
+++ ImprovisedIntoTheBreach/Program.cs
@@ -1,15 +1,20 @@
 ﻿global using Raylib_cs;
+global using System.Numerics;
+using ImprovisedIntoTheBreach;
 
 //Into the Breach with raylib
 //8x8 grid
 
-Raylib.InitWindow(800, 600, "hmm");
+int screenWidth = 1400;
+int screenHeight = 950;
+int gridSize = 8;
+
+Grid grid = new(new Vector2(screenWidth/2, screenHeight/2), gridSize, gridSize, 100);
+
+
+
+Raylib.InitWindow(screenWidth, screenHeight, "hmm");
 Raylib.SetTargetFPS(60);
-
-
-
-
-
 
 
 
@@ -18,11 +23,12 @@ Raylib.SetTargetFPS(60);
 while (!Raylib.WindowShouldClose())
 {
 
-
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.White);
 
-    Raylib.DrawGrid(100, 1f);
+    grid.Draw();
+
+    Raylib.DrawRectangle((int)grid.actualgrid[1,2].X, (int)grid.actualgrid[1,2].Y, 30, 30, Color.Red);
 
     Raylib.EndDrawing();
 }

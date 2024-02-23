1d245438c0ab58de5adc22d6bd06c63752990805
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2024-02-23 10:08:46 +0100

    Initial

---
+++ ImprovisedIntoTheBreach/Program.cs
@@ -0,0 +1,28 @@
+﻿global using Raylib_cs;
+
+//Into the Breach with raylib
+//8x8 grid
+
+Raylib.InitWindow(800, 600, "hmm");
+Raylib.SetTargetFPS(60);
+
+
+
+
+
+
+
+
+
+
+while (!Raylib.WindowShouldClose())
+{
+
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawGrid(100, 1f);
+
+    Raylib.EndDrawing();
+}
